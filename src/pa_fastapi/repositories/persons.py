b0a7a3bacb770dcc3d279375313aa91df0d8b26b
Volodymyr Zavatsky <volodymyr.zawatsky@gmail.com>
2023-09-25 20:21:22 +0300

    Change join of 'contacts' to left join. Add filter's criterion - by user_id column.

--- src/pa_fastapi/repositories/persons.py
+++ src/pa_fastapi/repositories/persons.py
@@ -1,5 +1,5 @@
 from datetime import date
-from sqlalchemy import func, and_, any_
+from sqlalchemy import func, and_, true
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -10,9 +10,9 @@ from schema import PersonContacts as Type
 
 
 
-async def reads_persons_by_birthday(days: int, session: Session) -> List[Type]:
+async def reads_persons_by_birthday(days: int, session: Session, user_id: int = 0) -> List[Type]:
     result = session.query(DBType)\
-                .join(DBType.contacts)\
+                .join(DBType.contacts, isouter=True)\
                 .filter(
                     and_(
                         DBType.born_date != None,
@@ -25,45 +25,73 @@ async def reads_persons_by_birthday(days: int, session: Session) -> List[Type]:
         persons.append(person)
     return persons
 
-async def reads_persons(first_name: str, last_name: str, session: Session) -> List[Type]:
+async def reads_persons(first_name: str, last_name: str, session: Session, user_id: int = 0) -> List[Type]:
     if first_name and last_name:
         result = session.query(DBType)\
-                    .join(DBType.contacts)\
+                    .join(DBType.contacts, isouter=True)\
                     .filter(
                         and_(
+                            DBType.user_id == user_id if user_id else any_(DBType.user_id),
                             DBType.first_name.like('%' + first_name + '%'),
                             DBType.last_name.like('%' + last_name + '%')
                         )
                     ).all()
     elif first_name:
         result = session.query(DBType)\
-                    .join(DBType.contacts)\
-                    .filter(DBType.first_name.like('%' + first_name + '%'))\
-                    .all()
+                    .join(DBType.contacts, isouter=True)\
+                    .filter(
+                        and_(
+                            DBType.user_id == user_id if user_id else any_(DBType.user_id),
+                            DBType.first_name.like('%' + first_name + '%')
+                        )
+                    ).all()
     elif last_name:
         result = session.query(DBType)\
-                    .join(DBType.contacts)\
-                    .filter(DBType.last_name.like('%' + first_name + '%'))\
-                    .all()
+                    .join(DBType.contacts, isouter=True)\
+                    .filter(
+                        and_(
+                            DBType.user_id == user_id if user_id else any_(DBType.user_id),
+                            DBType.last_name.like('%' + first_name + '%')
+                        )
+                    ).all()
     else:
+        if user_id:
+            criterion = DBType.user_id == user_id
+        else:
+            criterion = False
         result = session.query(DBType)\
-                    .join(DBType.contacts)
+                    .join(DBType.contacts, isouter=True )\
+                    .filter(criterion if id(criterion) != id(False) else true()).all()
     persons:List[Type] = []
     for r in result:
         person = Type.model_validate(r)
         persons.append(person)
     return persons
 
-async def reads_contacts(value: str, session: Session) -> List[Type]:
-    result = session.query(DBType).join(DBType.contacts).filter(Contact.value.like('%' + value + '%')).all()
+async def reads_contacts(value: str, session: Session, user_id: int = 0) -> List[Type]:
+    result = session.query(DBType)\
+                .join(DBType.contacts, isouter=True)\
+                .filter(
+                    and_(
+                        DBType.user_id == user_id if user_id else any_(DBType.user_id),
+                        Contact.value.like('%' + value + '%')
+                    )
+                ).all()
     contacts = []
     for r in result:
         contact = Type.model_validate(r)
         contacts.append(contact)
     return contacts
 
-async def read_contacts(pid: int, session: Session) -> List[Type]:
-    result = session.query(DBType).join(DBType.contacts).filter(DBType.id == pid).all()
+async def read_contacts(pid: int, session: Session, user_id: int = 0) -> List[Type]:
+    result = session.query(DBType)\
+                .join(DBType.contacts, isouter=True)\
+                .filter(
+                    and_(
+                        DBType.user_id  == user_id if user_id else any_(DBType.user_id),
+                        DBType.id       == pid
+                    )
+                ).all()
     contacts = []
     for r in result:
         contact = Type.model_validate(r)
