27adf90f754cb73f30eb4306aa77a5f8bdf453b1
Volodymyr Zavatsky <volodymyr.zawatsky@gmail.com>
2023-10-01 14:31:20 +0300

    Change path to alembic migrations.

--- src/pa_fastapi/database/alembic/env.py
+++ src/pa_fastapi/database/alembic/env.py
@@ -31,6 +31,7 @@ if config.config_file_name is not None:
 target_metadata = Base.metadata
 
 config.set_main_option("sqlalchemy.url", URL)
+config.set_main_option("script_location", current)
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
